99945cc9fba6b681fa62dec34c619a8df2dc7eff
janett22 <42725888+janett22@users.noreply.github.com>
2021-04-28 15:20:31 -0300

    Initial commit

---
+++ app/src/main/java/ui/MainActivity.java
@@ -0,0 +1,53 @@
+package ui;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.example.trabajofinal.R;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etUsuario, etContrasenia;
+    private Button btLogin;
+    private MainActivityViewModel vm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vm = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+        inicializarVista();
+        vm.getResultadoMutable().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String mensaje) {
+                new AlertDialog.Builder(getApplicationContext())
+                        .setTitle("Advertencia!")
+                        .setMessage(mensaje)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+
+                            }
+                        });
+            }
+        });
+
+    }
+    private void inicializarVista(){
+        etUsuario=findViewById(R.id.etUsuario);
+        etContrasenia=findViewById(R.id.etContrasenia);
+        btLogin=findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                vm.verificarDatos(etUsuario.getText().toString(),etContrasenia.getText().toString());
+            }
+        });
+    }
+}
